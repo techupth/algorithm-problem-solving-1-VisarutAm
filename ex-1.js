642386366105fb5c43102daf9449c2ea2d1dcd8b
Am Visarut <visarut.thk@gmail.com>
2024-07-14 10:11:12 +0700

    problem-solving

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,13 @@
 function twoSum(numbers, target) {
-	let num = {};
+	let storeNum = {};
     for (let i = 0; i < numbers.length; i++) {
         let result = target - numbers[i];
-	}
+        if (result in storeNum) {
+            return [storeNum[result], i];
+        }
+        storeNum[numbers[i]] = i;
+    }
+    return [];
 }
-numbers =  [2, 7, 11, 15]
+let numbers =  [2, 7, 11, 15]
 console.log(twoSum(numbers,9))
